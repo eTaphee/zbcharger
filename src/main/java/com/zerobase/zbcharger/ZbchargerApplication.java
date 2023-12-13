cd3a15442d26809cb40e11c46fdf0771c4136b1b
thlee <e-tap@naver.com>
2023-12-13 22:31:18 +0900

    Initial commit

---
+++ src/main/java/com/zerobase/zbcharger/ZbchargerApplication.java
@@ -0,0 +1,13 @@
+package com.zerobase.zbcharger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZbchargerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZbchargerApplication.class, args);
+	}
+
+}
