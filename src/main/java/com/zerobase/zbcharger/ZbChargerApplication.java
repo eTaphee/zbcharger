a335c9ea4f38c2d1db513590499df9e8fc3dc7d9
thlee <e-tap@naver.com>
2023-12-27 21:13:05 +0900

    refactor: 파일 이름 변경
    ZbchargerApplication -> ZbChargerApplication

---
+++ src/main/java/com/zerobase/zbcharger/ZbChargerApplication.java
@@ -0,0 +1,15 @@
+package com.zerobase.zbcharger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+@EnableAsync
+public class ZbChargerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZbChargerApplication.class, args);
+    }
+
+}
