24d0a86a07492efc59a7a6fff89165a3dd50b1d3
thlee <e-tap@naver.com>
2023-12-30 21:12:12 +0900

    feat: AuthService.authenticate() 이메일 인증 로직 추가

--- src/main/java/com/zerobase/zbcharger/domain/auth/service/AuthService.java
+++ src/main/java/com/zerobase/zbcharger/domain/auth/service/AuthService.java
@@ -1,10 +1,14 @@
 package com.zerobase.zbcharger.domain.auth.service;
 
 import static com.zerobase.zbcharger.exception.constant.ErrorCode.AUTHENTICATION_INVALID;
+import static com.zerobase.zbcharger.exception.constant.ErrorCode.EMAIL_VERIFICATION_NOT_FOUND;
+import static com.zerobase.zbcharger.exception.constant.ErrorCode.USER_NOT_VERIFIED;
 
 import com.zerobase.zbcharger.domain.auth.dto.AuthenticationDto;
 import com.zerobase.zbcharger.domain.auth.dto.GenerateTokenRequest;
+import com.zerobase.zbcharger.domain.member.dao.EmailVerificationRepository;
 import com.zerobase.zbcharger.domain.member.dao.MemberRepository;
+import com.zerobase.zbcharger.domain.member.entity.EmailVerification;
 import com.zerobase.zbcharger.domain.member.entity.Member;
 import com.zerobase.zbcharger.exception.CustomException;
 import lombok.RequiredArgsConstructor;
@@ -22,6 +26,8 @@ public class AuthService {
 
     private final MemberRepository memberRepository;
 
+    private final EmailVerificationRepository emailVerificationRepository;
+
     /**
      * 사용자 인증
      *
@@ -32,10 +38,25 @@ public class AuthService {
         Member member = memberRepository.findByEmail(request.email())
             .orElseThrow(() -> new CustomException(AUTHENTICATION_INVALID));
 
-        if (!passwordEncoder.matches(request.password(), member.getPassword())) {
-            throw new CustomException(AUTHENTICATION_INVALID);
-        }
+        checkPasswordMatched(request.password(), member.getPassword());
+        checkEmailVerified(member);
 
         return AuthenticationDto.fromEntity(member);
+    }
+
+    private void checkPasswordMatched(String rawPassword, String encodedPassword) {
+        if (!passwordEncoder.matches(rawPassword, encodedPassword)) {
+            throw new CustomException(AUTHENTICATION_INVALID);
+        }
+    }
+
+    private void checkEmailVerified(Member member) {
+        EmailVerification emailVerification = emailVerificationRepository
+            .findByMemberEmail(member.getEmail())
+            .orElseThrow(() -> new CustomException(EMAIL_VERIFICATION_NOT_FOUND));
+
+        if (!emailVerification.isVerifiedYn()) {
+            throw new CustomException(USER_NOT_VERIFIED);
+        }
     }
 }
